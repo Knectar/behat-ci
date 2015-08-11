110aab2f360c11644eabd7debf67c185bd268a9b
Jacob Geryk <jacobgeryk@gmail.com>
2015-08-11 11:10:57 -0400

    Fixed the ~/ bug with directory checking using ___FILE___

--- src/AppBundle/Commands/Schedule.php
+++ src/AppBundle/Commands/Schedule.php
@@ -21,7 +21,7 @@ class Schedule extends Command {
            ->addOption('e',
                         null,
                         InputOption::VALUE_OPTIONAL,
-                        'Environment. use -e=all for both dev and production',
+                        'Environment. use --e=all for both dev and production',
                         1
                       );
       }
