9993a07209034ca0c08db6e1e00deda1dc368147
Jacob Geryk <jacobgeryk@gmail.com>
2015-08-11 09:07:28 -0400

    adding clear command

---
+++ src/AppBundle/Commands/Clear.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace AppBundle\Commands;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Formatter\OutputFormatterStyle;
+
+class Clear extends Command {
+
+   //configuration of the command's name, arguments, options, etc
+    protected function configure()
+    {
+      $this->setName('clear')
+           ->setDescription("Clears the queue. Use flag --log to clear the log as well")
+           ->addOption(
+              'log',
+              null,
+              InputOption::VALUE_NONE,
+              'If set, clears the log as well../'
+           );
+      }
+
+    //executes code when command is called
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+      //Formatting terminal output
+      file_put_contents('/etc/bhqueue.txt', "");
+      $output->writeln('Queue Cleared');
+      if ($input->getOption('log')) {
+        file_put_contents('/etc/bhqueuelog.txt', "");
+        $output->writeln('Queue Log Cleared');
+       }
+      return 0;
+      }
+    }
