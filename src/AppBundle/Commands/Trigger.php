f977c42d0f666ebf68769d0df19168e2f7507c41
jake <Jacob Geryk>
2015-08-12 11:43:33 -0400

    Fixed bug where  wasn't being passed to matchandtest

--- src/AppBundle/Commands/Trigger.php
+++ src/AppBundle/Commands/Trigger.php
@@ -113,7 +113,7 @@ class Trigger extends Command {
           printf("Unable to parse the YAML string: %s", $e->getMessage());
       }
       //Generate the .yml config and run the tests
-      $this->matchAndTest($profiles, $projects, $projects);
+      $this->matchAndTest($profiles, $projects, $project);
     }
 
     protected function matchAndTest($profiles, $project, $projects){
