f193ec912d9013a83039e9e1d3e12cfd5ec3b163
jake <Jacob Geryk>
2015-08-17 11:18:32 -0400

    fixed test branch/profile handling logic

--- src/AppBundle/Commands/Test.php
+++ src/AppBundle/Commands/Test.php
@@ -21,15 +21,15 @@ class Test extends Trigger {
            ->addOption('branch',
                         'b',
                         InputOption::VALUE_OPTIONAL,
-                        'Environment. use --e=all for both dev and production',
+                        'Environment/branch. use --branch=all for both dev and production',
                         1
                       )
            ->addOption('profile',
                        'p',
                        InputOption::VALUE_OPTIONAL,
-                       'Environment. use --e=all for both dev and production',
+                       'Profile',
                        1
-                     );
+                      );
       }
 
     //executes code when command is called
@@ -39,22 +39,23 @@ class Test extends Trigger {
         $p=$input->getArgument('project_name');
         $b=$input->getOption('branch');
         $profile=$input->getOption('profile');
+        $output->writeln('profile: '.$profile);
         //Make sure the input is a proper environment
         if($b!='all' && $b!='dev' && $b!='production'){
           $output->writeln('<error>Please enter a valid environment! (dev, production, all)<error>');
-          $output->writeln($profile);
-          if(!$profile){
-            $this->bhTrigger($p, $e, $profile, $output);
+        } else {
+          if(!$profile==1){
+            $this->bhTrigger($p, $b, $profile, $output);
           } else {
-              if($e == 'all'){
+              if($b == 'all'){
                   //generates/runs tests for both dev and prod
                   $this->bhTrigger($p, 'dev', NULL, $output);
                   $this->bhTrigger($p, 'production', NULL, $output);
-              }else{
-                  $this->bhTrigger($p, $e, NULL, $output);
+              } else{
+                  $this->bhTrigger($p, $b, NULL, $output);
               }
           }
-          return 1;
         }
       }
+
     }
