cbea31373fb491ac962c5075a2185ff2aed0f108
Scrutinizer Auto-Fixer <auto-fixer@scrutinizer-ci.com>
2015-10-15 14:28:11 +0000

    Scrutinizer Auto-Fixes
    This commit consists of patches automatically generated for this project on https://scrutinizer-ci.com

--- src/AppBundle/Commands/BehatCi.php
+++ src/AppBundle/Commands/BehatCi.php
@@ -1,15 +1,7 @@
 <?php
 namespace AppBundle\Commands;
 
-use Symfony\Component\Console\Input\InputInterface;
-use Symfony\Component\Console\Output\OutputInterface;
-use Symfony\Component\Console\Input\InputArgument;
-use Symfony\Component\Console\Input\InputOption;
-use Symfony\Component\Console\Formatter\OutputFormatterStyle;
 use Symfony\Component\Yaml\Parser;
-use Symfony\Component\Yaml\Exception\ParseException;
-use Symfony\Component\Yaml\Dumper;
-use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
 
 /**
